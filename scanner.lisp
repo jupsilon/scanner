66eb4a7f8a8e9e9b9074bfe4fa7dccdce328aaae
Nakashima, Terumi <nasica@DESKTOP-9BP1GV3.localdomain>
2017-10-01 13:08:56 +0900

    ggg

--- scanner.lisp
+++ scanner.lisp
@@ -77,13 +77,6 @@
 				      (t item)))
 			      protocol)))
 
-(defun attribute (list key)
-  (cdr (assoc key
-	      (mapcar (lambda (item)
-			(unless (= (length item) 2) (error "bad attributes: ~a" list))
-			(cons (car item) (cadr item))) list)
-	      :test #'string=)))
-
 (defun trim-lines (lines)
   (remove-if (lambda (line) (string= line ""))
 	     (mapcar (lambda (line) (ppcre:regex-replace "^\\s+" line ""))
@@ -112,36 +105,13 @@
     (dolist (line (cdr lines))
       (<< header-rest line))))
 
-(defun emit-formal-parameters (args)
-  (if (null args) "(void) -> void"
-      (progn
-	(unless (string= (caar args) "arg") (error "missing arg"))
-	(let ((retn (find-if   (lambda (arg) (string= (attribute (cadr arg) "type") "new_id")) args))
-	      (prms (remove-if (lambda (arg) (string= (attribute (cadr arg) "type") "new_id")) args)))
-	  (if (null retn)
-	      (setf retn "void")
-	      (let ((interface-name (attribute (cadr retn) "interface")))
-		(if (null interface-name)
-		    (setf retn "void*")
-		    (setf retn (concat (attribute (cadr retn) "interface") "_t")))))
-	  (if (null prms)
-	      (setf prms "(void)")
-	      (progn
-		(setf prms (mapcar (lambda (arg) (if (string= (attribute (cadr arg) "type") "object")
-						     (concat  (attribute (cadr arg) "interface") "* "
-							      (attribute (cadr arg) "name"))
-						     (concat  (wire-type (attribute (cadr arg) "type")) " "
-							      (attribute (cadr arg) "name"))))
-				   prms))
-		(setf prms (format nil "(~{~a~^, ~})" prms))))
-	  (concat prms " -> " retn)))))
+(defun params-formal-parameters-list (params)
+  (mapcar (lambda (param) (concat (elt param 0) " " (elt param 1))) params))
 
-(defun emit-formal-parameters-list (params)
-  (if (null params)
+(defun emit-formal-parameters-list (formal-parameters-list)
+  (if (null formal-parameters-list)
       "()"
-      (format nil
-	      "(~{~a~^, ~})"
-	      (mapcar (lambda (param) (concat (elt param 0) " " (elt param 1))) params))))
+      (format nil "(~{~a~^, ~})" formal-parameters-list)))
 
 (defun emit-result-type (result)
   (if (null result)
@@ -153,7 +123,7 @@
 	    (let* ((attributes (node-get-attributes arg))
 		   (type       (attributes-get-value attributes "type")))
 	      (list (if (string= type "object")
-			(concat (attributes-get-value attributes "interface") "*")
+			(concat (attributes-get-value attributes "interface") "_t const&")
 			(wire-type type))
 		    (attributes-get-value attributes "name")
 		    (attributes-get-value attributes "summary"))))
@@ -170,18 +140,18 @@
 	      nil
 	      (attributes-get-value attributes "summary"))))))
 
-(defun emit-client-request-decl (member)
-  (indent/<<
-    (let* ((attributes   (node-get-attributes member))
-	   (name         (attributes-get-value attributes "name"))
-	   (type         (attributes-get-value attributes "type"))
-	   (children     (node-get-nodes member))
-	   (args         (nodes-select children "arg"))
-	   (params       (args-get-params args))
-	   (result       (args-get-result args))
-	   (descriptions (nodes-select children "description"))
-	   (brief        (node-get-brief (car descriptions)))
-	   (notes        (node-get-lines (car descriptions))))
+(defun emit-client-request-decl (request)
+  (let* ((attributes   (node-get-attributes request))
+	 (name         (attributes-get-value attributes "name"))
+	 (type         (attributes-get-value attributes "type"))
+	 (children     (node-get-nodes request))
+	 (args         (nodes-select children "arg"))
+	 (params       (args-get-params args))
+	 (result       (args-get-result args))
+	 (descriptions (nodes-select children "description"))
+	 (brief        (node-get-brief (car descriptions)))
+	 (notes        (node-get-lines (car descriptions))))
+    (indent/<<
       (when (not (null brief))
 	(<<       "/// @brief   " brief                         ))
       (dolist (param params)
@@ -201,30 +171,106 @@
       (emit-lines notes
 		  "/// @note    "
 		  "///          ")
-            (<<         (emit-result-type result) " " name (emit-formal-parameters-list params) ";"))))
-;      (<<         (emit-result-type result) " " name (emit-formal-parameters args) ";"))))
+      (let ((formal-list (params-formal-parameters-list params)))
+	(if (and (not (null type)) (string= type "destructor"))
+	    (<<   (emit-result-type result) " " name (emit-formal-parameters-list formal-list) ";")
+	    (<<   (emit-result-type result) " " name (emit-formal-parameters-list formal-list) " const;"))))))
 
-(defun emit-client-interface-members (interface-members)
-  (let ((requests (nodes-select interface-members "request")))
+(defun params-parameter-types-list (params)
+  (mapcar (lambda (param) (elt param 0)) params))
+
+(defun emit-parameter-types-list (parameter-types-list)
+  (if (null parameter-types-list)
+      "()"
+      (format nil "(~{~a~^, ~})" parameter-types-list)))
+
+(defun emit-client-listener-handler-binder (events)
+  (indent/<<
+    (dolist (event events)
+      (let* ((name (node-get-value event "name")))
+	(<< "// " name)))))
+
+(defun emit-client-listener-handler (event interface-name)
+  (let* ((name         (node-get-value event "name"))
+	 (children     (node-get-nodes event))
+	 (args         (nodes-select children "arg"))
+	 (params       (args-get-params args))
+	 (result       (args-get-result args))
+	 (descriptions (nodes-select children "description"))
+	 (brief        (node-get-brief (car descriptions)))
+	 (notes        (node-get-lines (car descriptions)))
+	 (formal-list  (cons "void* data" (cons (concat interface-name "_t const& self") (params-formal-parameters-list params))))
+	 (types-list   (cons "void*" (cons (concat interface-name "_t const&") (params-parameter-types-list params)))))
+    (indent/<<
+      (when (not (null brief))
+	(<<       "/// @brief   " brief                         ))
+      (dolist (param params)
+	(let ((param-name    (elt param 1))
+	      (param-summary (elt param 2)))
+	  (if (null param-summary)
+	      (<< "/// @param   " param-name                    )
+	      (<< "/// @param   " param-name " " param-summary  ))))
+      (when (not (null result))
+	(let ((result-type    (elt result 0))
+	      (result-summary (elt result 2)))
+	  (<<     "/// @return  " result-type                   )
+	  (when (not (null result-summary))
+	    (<<   "/// @retval  " result-summary                ))))
+      (emit-lines notes
+		  "/// @note    "
+		  "///          ")
+      (<<         "virtual " (emit-result-type result) " " name (emit-formal-parameters-list formal-list) " const;")
+      (<<)
+      (<<         "/// @brief   delegation for " name)
+      (<<         "delegation<" (emit-result-type result) " " (emit-parameter-types-list types-list) "> " name "_delegation;"))))
+
+(defun emit-client-interface-members (interface-members interface-name)
+  (let ((requests (nodes-select interface-members "request"))
+	(events   (nodes-select interface-members "event")))
     (when (not (null requests))
       (<<          )
-      (<< "public:")
+      (<<     "public:")
       (format (current-output)
 	      "~{~a~^~%~}"
-	      (mapcar (lambda (request) (s/<< (emit-client-request-decl request))) requests)))))
+	      (mapcar (lambda (request) (s/<< (emit-client-request-decl request))) requests)))
+    (when (not (null events))
+      (<<)
+      (<<     "public:")
+      (indent/<<
+	(<<     "class listener {")
+	(<<     "public:")
+	(indent/<<
+	  (<<     "listener(void* data = nullptr)")
+	  (<<     "  : data_(data)")
+	  (<<     "{")
+	  (indent/<<
+	    (<<     "this->listener_ = " interface-name "_listener {")
+	    (emit-client-listener-handler-binder events)
+	    (<<     "};"))
+	  (<<     "}"))
+	(<<)
+	(<<     "public:")
+	(format (current-output)
+		"~{~a~^~%~}"
+		(mapcar (lambda (event) (s/<< (emit-client-listener-handler event interface-name))) events))
+	(<<)
+	(<<     "private:")
+	(indent/<<
+	  (<<    interface-name "_listener listener_;"))
+	(<<     "};")))))
 
 (defun emit-client-interface (interface)
-  (indent/<<
-    (let* ((attributes   (node-get-attributes interface))
-	   (name         (attributes-get-value attributes "name"))
-	   (version      (attributes-get-value attributes "version"))
-	   (children     (node-get-nodes interface))
-	   (members      (nodes-select-not children "description"))
-	   (descriptions (nodes-select     children "description"))
-	   (brief        (node-get-brief (car descriptions)))
-	   (notes        (node-get-lines (car descriptions)))
-	   (class        (concat name "_t"))
-	   (base-class   (concat "wayland_client_core<" class ", " version ">")))
+  (let* ((attributes   (node-get-attributes interface))
+	 (name         (attributes-get-value attributes "name"))
+	 (version      (attributes-get-value attributes "version"))
+	 (children     (node-get-nodes interface))
+	 (members      (nodes-select-not children "description"))
+	 (descriptions (nodes-select     children "description"))
+	 (brief        (node-get-brief (car descriptions)))
+	 (notes        (node-get-lines (car descriptions)))
+	 (class        (concat name "_t"))
+	 (base-class   (concat "wayland_client_core<" class ", " version ">")))
+    (indent/<<
       (<<         "/// @class " class                         )
       (when (not (null brief))
 	(<<       "/// @brief " brief                         ))
@@ -240,18 +286,68 @@
       (<<         "public:"                                   )
       (indent/<<
 	(<<         "/// @constructor"                        )
-	(<<         "/// @param src the source pointer"       )
+	(<<         "/// @param src source raw pointer"       )
 	(<<          class "(c_struct_type* src = nullptr)"   )
 	(<<         "  : base_type(src)"                      )
 	(<<         "{"                                       )
 	(<<         "}"                                       ))
-      (emit-client-interface-members members)
+      (emit-client-interface-members members name)
       (<<         "};"                                        ))))
+
+(defun params-actual-arguments-list (params)
+  (mapcar (lambda (param) (elt param 1)) params))
+
+(defun emit-actual-arguments-list (actual-arguments-list)
+  (if (null actual-arguments-list)
+      ""
+      (format nil "(~{~a~^, ~})" actual-arguments-list)))
+
+(defun emit-client-request-impl (request interface-name)
+  (let* ((attributes   (node-get-attributes request))
+	 (name         (attributes-get-value attributes "name"))
+	 (type         (attributes-get-value attributes "type"))
+	 (children     (node-get-nodes request))
+	 (args         (nodes-select children "arg"))
+	 (params       (args-get-params args))
+	 (result       (args-get-result args))
+	 (formal-list  (params-formal-parameters-list params))
+	 (actual-list  (cons "this->get()" (params-actual-arguments-list params))))
+    (if (and (not (null type)) (string= type "destructor"))
+	(<< "inline " (emit-result-type result) " " interface-name "_t::" name (emit-formal-parameters-list formal-list) " {")
+	(<< "inline " (emit-result-type result) " " interface-name "_t::" name (emit-formal-parameters-list formal-list) " const {"))
+    (indent/<<
+      (<< "return " interface-name "_" name (emit-actual-arguments-list actual-list) ";"))
+    (<< "}")))
+
+(defun emit-client-interface-members-impl (interface-members interface-name)
+  (let ((requests (nodes-select interface-members "request")))
+    (format (current-output)
+	    "~{~a~^~%~}"
+	    (mapcar (lambda (request) (s/<< (emit-client-request-impl request interface-name))) requests))))
+
+(defun emit-client-interface-impl (interface)
+  (let ((name    (node-get-value interface "name"))
+	(members (nodes-select-not (node-get-nodes interface) "description")))
+    (indent/<<
+      (<< "//")
+      (<< "// implementaions for " name)
+      (<< "//")
+      (emit-client-interface-members-impl members name))))
 
 (defun emit-client-interfaces (interfaces)
   (format (current-output)
 	  "~{~a~^~%~}"
 	  (mapcar (lambda (interface) (s/<< (emit-client-interface interface))) interfaces)))
+
+(defun emit-client-interfaces-decl (interfaces)
+  (indent/<<
+    (dolist (interface interfaces)
+      (<< "class " (node-get-value interface "name") ";"))))
+
+(defun emit-client-interfaces-impl (interfaces)
+  (format (current-output)
+	  "~{~a~^~%~}"
+	  (mapcar (lambda (interface) (s/<< (emit-client-interface-impl interface))) interfaces)))
 
 (defun emit-client (root)
   (let ((protocol-name (node-get-value root "name"))
@@ -270,20 +366,26 @@
       (emit-lines rights-notes
 		  "/// @copyright "
 		  "///            ")
-      (<<         ""                                                               )
+      (<<                                                                          )
       (<<         "#ifndef INCLUDE_" (string-upcase protocol-name) "_CLIENT_HPP_"  )
       (<<         "#define INCLUDE_" (string-upcase protocol-name) "_CLIENT_HPP_"  )
-      (<<         ""                                                               )
-      (<<         "namespace " protocol-name "_client"                             )
+      (<<                                                                          )
+      (<<         "namespace wayland_client_wrappers"                              )
       (<<         "{"                                                              )
-      (emit-client-interfaces interfaces)
-      (<<         "}"                                                              )
-      (<<         ""                                                               )
+      (when (not (null interfaces))
+	(emit-client-interfaces-decl interfaces)
+	(<<)
+	(emit-client-interfaces interfaces)
+	(<<)
+	(emit-client-interfaces-impl interfaces))
+      (<<         "} // namespace wayland_client_wrappers"                         )
+      (<<                                                                          )
       (<<         "#endif/*INCLUDE_" (string-upcase protocol-name) "_CLIENT_HPP_*/"))))
 
 ;(emit-client (preprocess (cxml:parse      *standard-input*           (cxml-xmls:make-xmls-builder))))
 
-;(set-indent-unit #\Tab)
+(set-indent-unit "    ")
 ;(emit-client (preprocess (cxml:parse-file "weston-desktop-shell.xml"         (cxml-xmls:make-xmls-builder))))
 ;(emit-client (preprocess (cxml:parse-file "wayland.xml"                      (cxml-xmls:make-xmls-builder))))
-(emit-client (preprocess (cxml:parse-file "relative-pointer-unstable-v1.xml" (cxml-xmls:make-xmls-builder))))
+;(emit-client (preprocess (cxml:parse-file "relative-pointer-unstable-v1.xml" (cxml-xmls:make-xmls-builder))))
+(emit-client (preprocess (cxml:parse-file "xdg-foreign-unstable-v1.xml"      (cxml-xmls:make-xmls-builder))))
